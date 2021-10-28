378ff4ad3ab46e9f3f1173277d2fd79eacb232c9
Guilherme <guilhermebernava00@gmail.com>
2021-10-28 15:59:36 -0300

    Funcionando V2 eu acho, 23,09% de certeza

---
+++ TryCatchExercise/Entities/Account.cs
@@ -0,0 +1,103 @@
+﻿using Baseline.ImTools;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using TryCatchExercise.Entities.Execptions;
+
+namespace TryCatchExercise.Entities
+{
+    internal class Account
+    {
+        public double _balance;
+        public string _holder;
+        public double _withDrawLimit;
+        
+        public int Number { get; set; }
+
+        public double WithDrawLimit
+        {
+            get { return _withDrawLimit; }
+            set
+            {
+                if (_withDrawLimit < 0)
+                {
+                    throw new DomainExecption("Withdraw limit is less than 0");
+                }
+                else if (_withDrawLimit >= _balance)
+                {
+                    throw new DomainExecption("Withdraw limit is more than balance");
+                }
+                _withDrawLimit = value;
+            }
+        }
+
+        public string Holder
+        {
+            get { return _holder; }
+            
+            set{_holder = value;}
+        }
+
+        public double Balance
+        {
+            get { return _balance; }
+            set
+            {
+                if (value <= 0 && value != null)
+                {
+                    throw new DomainExecption("Error: Balance has to be more than 0 Dollars");
+                }
+                _balance = value;
+            }
+
+        }
+
+        public Account() {
+        }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            _holder = holder;
+            _balance = balance;
+            _withDrawLimit = withDrawLimit;
+        }
+
+        public void deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+
+            if (WithDrawLimit < amount)
+            {
+                throw new DomainExecption("Amount bigger than Withdraw limit");
+            }
+            if (amount < 0)
+            {
+                throw new DomainExecption("Amount is negative");
+            }
+            Balance -= amount;
+        }
+
+        public void TesteHolderHasInt()
+        {
+            bool containsInt = _holder.Any(char.IsDigit);
+            if (Regex.Match(_holder, @"\b[0-9]\w").Success)
+            {
+                throw new DomainExecption("Has a number in Holder");
+            }
+            if (containsInt)
+            {
+                throw new DomainExecption("Has a number in Holder");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"New balance: {Balance}";
+        }
+    }
+}
---
+++ TryCatchExercise/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using TryCatchExercise.Entities;
+using TryCatchExercise.Entities.Execptions;
+
+namespace TryCatchExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter account data");
+                    Console.Write("Number: ");
+                    acc.Number = Int32.Parse(Console.ReadLine());
+                    Console.Write("Holder: ");
+                    acc.Holder = Console.ReadLine();
+                    acc.TesteHolderHasInt();
+                    Console.Write("Initial Balance: ");
+                    acc.Balance = double.Parse(Console.ReadLine());
+                    Console.Write("Withdraw limit: ");
+                    acc.WithDrawLimit = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (DomainExecption e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine();
+
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Error Type: " + e.Message);
+                    Console.WriteLine();
+                }
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.Write("Enter amount for withdraw: ");
+                    double withdraw = double.Parse(Console.ReadLine());
+                    acc.WithDraw(withdraw);
+                    Console.WriteLine(acc);
+                    break;
+                }
+                catch (DomainExecption e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+
+                }
+            }
+        }
+    }
+}
